a4d4e98bf8a3c380920b5c77df7e52a5b6a23e30
Antoniofrancosilva <antonio.contato0301@gmail.com>
2026-07-03 13:03:37 -0300

    Update ler_dbf_service.py

--- backend/services/sincronizacao/ler_dbf_service.py
+++ backend/services/sincronizacao/ler_dbf_service.py
@@ -1,11 +1,13 @@
-class LerDBFService:
+from dbfread import DBF
 
+class LerDBFService:
+ 
     def executar(self, caminho_arquivo):
         """
         Recebe o caminho de um arquivo .dbf,
         lê os registros e os transforma em
         uma lista de dicionários.
-
+ 
         Retorno esperado:
             [
                 {
@@ -15,4 +17,22 @@ class LerDBFService:
                 }
             ]
         """
-        pass
+        tabela = DBF(caminho_arquivo, encoding="utf-8", load=True)
+ 
+        return [
+            {
+                "id_anm": registro.get("ID"),
+                "processo": registro.get("PROCESSO"),
+                "numero": registro.get("NUMERO"),
+                "ano": registro.get("ANO"),
+                "area_ha": registro.get("AREA_HA"),
+                "fase": registro.get("FASE"),
+                "ult_evento": registro.get("ULT_EVENTO"),
+                "nome": registro.get("NOME"),
+                "subs": registro.get("SUBS"),
+                "uso": registro.get("USO"),
+                "uf": registro.get("UF"),
+                "ds_processo": registro.get("DSProcesso"),
+            }
+            for registro in tabela
+        ] #acho que era pra fazer isso
