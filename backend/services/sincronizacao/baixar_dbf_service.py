fff546f55dc87c90909d68d295e3f4f31b8411cc
Antoniofrancosilva <antonio.contato0301@gmail.com>
2026-07-03 13:02:44 -0300

    Update baixar_dbf_service.py

--- backend/services/sincronizacao/baixar_dbf_service.py
+++ backend/services/sincronizacao/baixar_dbf_service.py
@@ -1,11 +1,25 @@
-class BaixarDBFService:
+import requests
 
-    def executar(self):
-        """
-        Responsável por acessar o site da ANM e baixar
-        o arquivo DBF mais recente.
+url = "https://geo.anm.gov.br/arcgis/rest/services/SIGMINE/dados_anm/MapServer/0/query"
 
-        Retorno esperado:
-            caminho do arquivo salvo localmente.
-        """
-        pass
+params = {
+    "where": "1=1",
+    "outFields": "*",
+    "returnGeometry": False,
+    "f": "json"
+}
+
+r = requests.get(url, params=params, timeout=60)
+r.raise_for_status()
+
+dados = r.json()
+
+print(f"Total recebido: {len(dados['features'])}")
+print()
+
+for i, feature in enumerate(dados["features"][:1000], start=1):#todos = 5000 / colocar filtro UF = MG
+    
+    print(f"Registro {i}")
+    for campo, valor in feature["attributes"].items():
+        print(f"{campo}: {valor}")
+    print("-" * 50)
